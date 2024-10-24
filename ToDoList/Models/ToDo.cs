b9a9074a57127a36fe4ce334547edfcc20ff74ab
tommy w <tlwjr27@gmail.com>
2024-10-24 11:38:42 -0500

    added valiadtion

--- ToDoList/Models/ToDo.cs
+++ ToDoList/Models/ToDo.cs
@@ -11,14 +11,16 @@ namespace ToDoList.Models
         [Required(ErrorMessage = "Please enter a description for the task.")]
         public string Description { get; set; } = string.Empty;
         [Required(ErrorMessage ="Please enter a due date.")]
+        [YearFromNow(100, IsPast = true)]
         public DateTime? DueDate { get; set; }
         [Required(ErrorMessage = "Please enter sprint number for task.")]
         public int? SprintNumber { get; set; } = null!;
         [Required(ErrorMessage = "Please enter the point value for the task.")]
+        [[Range(0 , 250, ErrorMessage ="Pleaes enter a point value between 0 and 250.")]]
         public int? PointValue { get; set; } = null!;
         [Required(ErrorMessage = "Please select a status.")]
         public string StatusID { get; set; } = string.Empty;
-        [ValidateNever]
+       
         public Status Status { get; set; } = null!;
 
         public bool Overdue => StatusID != "done" && DueDate < DateTime.Today;
---
+++ ToDoList/Models/YearFromNow.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.RateLimiting;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class YearFromNow : ValidationAttribute
+    {
+        private int numYears;
+        public YearFromNow(int years)
+        {
+            numYears = years;
+        }
+        public bool IsPast { get; set; } = false;
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+           if (value is DateTime)
+            {
+                DateTime dateToCheck = (DateTime)value;
+
+                DateTime now = DateTime.Today;
+                DateTime limit;
+
+                if(IsPast)
+                {
+                    limit = new DateTime(now.Year, 1, 1);
+                    limit = limit.AddYears(-numYears);
+                } 
+                else
+                {
+                    limit = new DateTime(numYears, 12, 31);
+                    limit = limit.AddYears(numYears);
+                }
+
+                if (IsPast)
+                {
+                    if (dateToCheck >= limit && dateToCheck < now)
+                    {
+                        return ValidationResult.Success!;
+                    }
+                } 
+                else
+                {
+                    if (dateToCheck > now && dateToCheck <= limit)
+                    {
+                        return ValidationResult.Success!;
+                    }
+                }
+            }
+
+            string msg = base.ErrorMessage ?? validationContext.DisplayName + " must be a " + (IsPast ? "past" : "future") + " date within " + numYears + " years of now.";
+            return new ValidationResult(msg);
+        }
+    }
+}
