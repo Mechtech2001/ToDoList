6243943ab7899cccf4f164d0a39e0ce10e131c37
tommy w <tlwjr27@gmail.com>
2024-11-21 09:47:27 -0600

    added view components and tag helper

---
+++ ToDoList/ViewComponents/StatusActionsViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models; 
+
+namespace ToDoList.ViewComponents
+{
+    public class TaskStatusActionsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ToDo task)
+        {
+            return View(task);
+        }
+    }
+}
